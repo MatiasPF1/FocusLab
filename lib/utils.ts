9feb158954baf9aaff7fadff1ce2ce949d2a38f2
Matias Freire <matiaspfreire@gmail.com>
2026-03-03 08:49:42 -0500

    schema done

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-
+// Don't erase, is used in components/ui/flow-field-background.tsx that filters out any null, undefined, or false values.
 export function cn(...classes: (string | undefined | null | false)[]) {
   return classes.filter(Boolean).join(' ');
 }
